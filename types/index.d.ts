5b9243b39d14e79dde8dd60d7f17b3970a3bc41f
devspagnuolo <rafaelspagnuologarcia@gmail.com>
2025-04-01 23:48:02 -0300

    Update index.d.ts

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,7 +1,9 @@
 declare global {
   namespace Express {
     interface Request {
-      user?: { id: number } & Partial<User>;
+      user?: {
+        id: number;
+      } & Partial<User>; // `User` precisa ser declarado ou importado, caso esteja sendo usado.
     }
   }
 }
